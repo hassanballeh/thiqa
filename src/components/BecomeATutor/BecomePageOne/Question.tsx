de75e9d7aa33be985e90232b2b0928a0adcc4658
MohammadRL <mohammad.alrahhal.01@gmail.com>
2025-09-17 13:10:54 +0400

    Become A Tutor (How to) page updates

--- src/components/BecomeATutor/BecomePageOne/Question.tsx
+++ src/components/BecomeATutor/BecomePageOne/Question.tsx
@@ -1,48 +1,49 @@
-"use client"
-import { AccordionItem } from '@/components/Custom/Accordion';
-import React, { useState } from 'react'
-import { useTranslation } from 'react-i18next';
+"use client";
+import { AccordionItem } from "@/components/Custom/Accordion";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const Question = () => {
-          const { t } = useTranslation();
-  const [openIndex, setOpenIndex] = useState<number | null>(null)
+  const { t } = useTranslation();
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const toggleAccordion = (index: number) => {
-    setOpenIndex((prevIndex) => (prevIndex === index ? null : index))
-  }
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
 
-const items = [
-  {
-    title: t("become.21-questions1"),
-    content: t("become.21-answer1"),  
-  },
-  {
-    title: t("become.21-questions2"),
-    content: t("become.21-answer2"),  
-  },
+  const items = [
     {
-    title: t("become.21-questions3"),
-    content: t("become.21-answer3"),  
-  },
+      title: t("become.21-questions1"),
+      content: t("become.21-answer1"),
+    },
     {
-    title: t("become.21-questions4"),
-    content: t("become.21-answer4"),  
-  },
+      title: t("become.21-questions2"),
+      content: t("become.21-answer2"),
+    },
     {
-    title: t("become.21-questions5"),
-    content: t("become.21-answer5"),  
-  },
-];
+      title: t("become.21-questions3"),
+      content: t("become.21-answer3"),
+    },
+    {
+      title: t("become.21-questions4"),
+      content: t("become.21-answer4"),
+    },
+    {
+      title: t("become.21-questions5"),
+      content: t("become.21-answer5"),
+    },
+  ];
   return (
     <div className="max-w-6xl mx-auto px-4 py-20">
       <section className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center text-gray1">
         {/* Text Section */}
         <div className="text-2xl md:text-4xl  max-w-md flex flex-col justify-center text-center md:text-start">
           <h2 className="font-bold text-primary leading-relaxed ">
-                        {t("become.21-heading1")}
+            {t("become.21-heading1")}
           </h2>
-          <p className="text-sm font-light mt-4 leading-relaxed ">
-{t("become.21-heading2")}          </p>
+          <p className="text-sm font-light mt-4 leading-relaxed md:max-w-[90%]">
+            {t("become.21-heading2")}{" "}
+          </p>
         </div>
 
         {/* Accordion Section */}
@@ -59,7 +60,7 @@ const items = [
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
--- src/components/BecomeATutor/BecomePageOne/Steps.tsx
+++ src/components/BecomeATutor/BecomePageOne/Steps.tsx
@@ -1,120 +1,123 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 const Steps = () => {
-              const { t } = useTranslation();
+  const { t } = useTranslation();
   const stepsData = [
-  {
-    number: 1,
-    title: t("become.17-number1-1"),
-    desc: t("become.17-number1-2"),
-  },
-  {
-    number: 2,
-    title: t("become.17-number2-1"),
-    desc: t("become.17-number2-2"),
-   },
-  {
-    number: 3,
-    title: t("become.17-number3-1"),
-    desc: t("become.17-number3-2"),
-   },
-  {
-    number: 4,
-    title: t("become.17-number4-1"),
-    desc: t("become.17-number4-2"),
+    {
+      number: 1,
+      title: t("become.17-number1-1"),
+      desc: t("become.17-number1-2"),
     },
-  {
-    number: 5,
-    title: t("become.17-number5-1"),
-    desc: t("become.17-number5-2"),
-   },
-  {
-    number: 6,
-    title: t("become.17-number6-1"),
-    desc: t("become.17-number6-2"),
+    {
+      number: 2,
+      title: t("become.17-number2-1"),
+      desc: t("become.17-number2-2"),
     },
-];
+    {
+      number: 3,
+      title: t("become.17-number3-1"),
+      desc: t("become.17-number3-2"),
+    },
+    {
+      number: 4,
+      title: t("become.17-number4-1"),
+      desc: t("become.17-number4-2"),
+    },
+    {
+      number: 5,
+      title: t("become.17-number5-1"),
+      desc: t("become.17-number5-2"),
+    },
+    {
+      number: 6,
+      title: t("become.17-number6-1"),
+      desc: t("become.17-number6-2"),
+    },
+  ];
   return (
-<div className="py-12 bg-primary">
-  <section className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 px-6 py-12">
-    
-    <div className="md:col-span-1 flex flex-col justify-center items-center">
-      <div className="text-white md:px-4 text-center md:text-start flex flex-col justify-center max-w-sm">
-        <h2 className="text-3xl font-semibold leading-relaxed">
-          {t("become.17-heading1")}
-        </h2>
-        <p className="text-[15px] font-light leading-relaxed mt-4 max-w-sm">
-{t("become.17-heading2")}        </p>
+    <div className="py-12 bg-primary">
+      <section className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 px-6 py-12">
+        <div className="md:col-span-1 flex flex-col justify-center items-center">
+          <div className="text-white md:px-4 text-center md:text-start flex flex-col justify-center max-w-sm">
+            <h2 className="text-3xl font-semibold leading-relaxed md:whitespace-pre-line">
+              {t("become.17-heading1")}
+            </h2>
+            <p className="text-[15px] font-light leading-relaxed mt-1 max-w-sm">
+              {t("become.17-heading2")}{" "}
+            </p>
 
-        <div className="flex flex-col gap-4 mt-6 mx-auto md:mx-0 items-start">
-          <div className="flex gap-4 mx-auto md:mx-0">
-            <Link
-            href="/under-develop"
-              className="bg-gold rounded-3xl px-10 font-semibold py-1.5 hover:bg-gold"
-            >
-              <span>Sign up as a tutor</span>
-            </Link>
+            <div className="flex flex-col gap-4 mt-6 mx-auto md:mx-0 items-start">
+              <div className="flex gap-4 mx-auto md:mx-0">
+                <Link
+                  href="/under-develop"
+                  className="bg-gold rounded-3xl px-10 font-semibold py-1.5 hover:bg-gold"
+                >
+                  <span>Sign up as a tutor</span>
+                </Link>
+              </div>
+
+              <Link
+                href="/under-develop"
+                className="flex items-center underline font-medium hover:text-blue-100 text-sm"
+              >
+                <span>View benefits & earnings</span>
+                <svg
+                  className="w-4 h-4 ml-1"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  ></path>
+                </svg>
+              </Link>
+            </div>
           </div>
-
-          <Link
-            href="/under-develop"
-            className="flex items-center underline font-medium hover:text-blue-100 text-sm"
-          >
-            <span>View benefits & earnings</span>
-            <svg
-              className="w-4 h-4 ml-1"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"></path>
-            </svg>
-          </Link>
         </div>
-      </div>
-    </div>
-<div className="md:col-span-2 flex flex-wrap gap-4 mt-10 justify-center ">
-  {[0, 1].map((colIdx) => (
-    <div key={colIdx} className="flex flex-col gap-4 items-start">
-      {stepsData
-        .filter((_, i) => i % 2 === colIdx)
-        .map((step) => (
-          <div
-            key={step.number}
-            className="
+        <div className="md:col-span-2 flex flex-wrap lg:flex-nowrap gap-4 mt-10 justify-center ">
+          {[0, 1].map((colIdx) => (
+            <div key={colIdx} className="flex flex-col gap-4 items-start">
+              {stepsData
+                .filter((_, i) => i % 2 === colIdx)
+                .map((step) => (
+                  <div
+                    key={step.number}
+                    className="
               bg-primaryDark p-4 md:py-8 rounded-3xl min-h-40
-              flex flex-col md:flex-row items-center md:gap-4
+              flex flex-col md:flex-row items-center md:items-start md:gap-4
               transition-transform duration-300 ease-in-out
               hover:scale-105 hover:shadow-xl 
               w-auto
             "
-          >
-            <span className="text-3xl md:text-6xl font-medium text-gold min-w-[60px] md:min-w-[80px] text-center tabular-nums">
-              {String(step.number).padStart(2, "0")}
-            </span>
+                  >
+                    <span className="text-3xl md:text-6xl font-medium text-gold min-w-[60px] md:min-w-[80px] text-center tabular-nums self-center">
+                      {String(step.number).padStart(2, "0")}
+                    </span>
 
-            <div className="text-center md:text-left mt-2 md:mt-0">
-              <h4 className="text-sm md:text-[16px] font-semibold text-white leading-snug">
-                {step.title}
-              </h4>
-              <p className="text-xs md:text-xs font-extralight text-white mt-2 md:mt-3 max-w-60">
-                <span className="leading-relaxed">{step.desc}</span>
-              </p>
+                    <div className="text-center md:text-left mt-2 md:mt-0">
+                      <h4 className="text-sm md:text-[16px] font-semibold text-white leading-snug">
+                        {step.title}
+                      </h4>
+                      <p className="text-xs md:text-xs font-extralight text-white mt-2 md:mt-3 max-w-60 lg:min-h-[56px]">
+                        <span className="leading-relaxed">{step.desc}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      </section>
     </div>
-  ))}
-</div>
+  );
+};
 
-  </section>
-</div>
-  )
-}
-
-export default Steps
+export default Steps;
